6eac36fec63c313be37c16907009031dffb5ed9d
绝伦N <56631419+404name@users.noreply.github.com>
2022-06-14 19:02:36 +0800

    Update index.js

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -3,4 +3,58 @@ module.exports = [
     { text: '起步', link: '/welcome/before_learning' },
     { text: 'Github', link: 'http://www.baidu.com' },
     { text: 'MySQL', link: '/sql/sql1' },
+	{
+        text: '碎碎念', link: '/coding/',
+		items: [
+            {text: 'PAT', link: '/baodian/zero/'},
+			{text: '剑指offer', link: '/baodian/zero/'},
+            {text: 'LeeCode', link: '/baodian/high/'}
+        ]
+    },
+	{
+        text: '爱编程', link: '/coding/',
+		items: [
+            {text: 'PAT', link: '/coding/pat/'},
+			{text: '剑指offer', link: '/coding/offer/'},
+            {text: 'LeeCode', link: '/coding/leeCode/'}
+        ]
+    },
+	{
+        text: '专栏', link: '/column/',
+		items: [
+            {text: '群晖NAS Docker系列教程', link: '/column/synology/'},
+			{text: 'vuepress系列教程', link: '/column/vuepress/'},
+            {text: 'springboot系列教程', link: '/column/springboot/'}
+        ]
+    },
+    {
+        text: '后端',
+        items: [
+			{
+                text: 'Mysql',
+				items: [
+					{text: 'DS918-Mysql', link: 'http://mysql.hepcloud.top'},
+					{text: 'DS220-MySql', link: 'http://mysql.huerpu.top'}
+				]
+            },
+			{
+                text: 'Redis',
+				items: [
+					{text: 'DS918-Redis', link: 'http://redis.hepcloud.top'},
+					{text: 'DS220-Redis', link: 'http://redis.huerpu.top'}
+				]
+            },
+			{
+                text: 'ElasticSearch',
+				items: [
+					{text: 'DS918-es', link: 'http://es.hepcloud.top'},
+					{text: 'DS220-es', link: 'http://es.hepcloud.top'}
+				]
+            }
+        ]
+    },
+	{
+        text: '关于我', link: 'http://www.yuque.com/ctguqmx'
+    }
 ]
+
